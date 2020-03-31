611bb026777d0c2a0f8bc61cf21d16b418e12a42
Rahat Hossain <rahathossain690@gmail.com>
2020-03-31 08:23:16 +0600

    new branch

---
+++ Frontend/ASSETS/JS/signin.js
@@ -0,0 +1,48 @@
+
+// navigation button part
+
+document.getElementById("navbtn_profile").addEventListener("click", noReturn => {
+	window.location = "profile.html"; // temporary
+});
+
+document.getElementById("navbtn_practice").addEventListener("click", noReturn => {
+	window.location = "practice.html"; // temporary
+});
+
+document.getElementById("navbtn_signout").addEventListener("click", noReturn => {
+	
+});
+
+// check for log in
+
+
+// sign in
+document.getElementById("signin").addEventListener("click", noReturn => {
+	let email = document.getElementById("email").value.trim();
+	let pass = document.getElementById("pass").value.trim();
+	if(email == "" || pass == "") {
+		showAlert("Wrong inputs.")
+	} else {
+		hideAlert();
+		// sign in try
+	}
+});
+
+function showAlert(text){
+	document.getElementById("alert").classList.remove("hide");
+	document.getElementById("alerttext").innerHTML = text;
+}
+
+function hideAlert(){
+	document.getElementById("alert").classList.add("hide");
+	document.getElementById("alerttext").innerHTML = "";
+}
+
+// forgot password
+document.getElementById("forgot_pass").addEventListener("click", noReturn => {
+	let email = document.getElementById("email").value.trim();
+	if(email == "") showAlert("Email field is necessary.");
+	else {
+		// go for it
+	}
+});
---
+++ Frontend/ASSETS/JS/signup.js
@@ -0,0 +1,27 @@
+
+// navigation button part
+
+document.getElementById("navbtn_profile").addEventListener("click", noReturn => {
+	window.location = "profile.html"; // temporary
+});
+
+document.getElementById("navbtn_practice").addEventListener("click", noReturn => {
+	window.location = "practice.html"; // temporary
+});
+
+document.getElementById("navbtn_signout").addEventListener("click", noReturn => {
+	// log out
+});
+
+// check for log in
+
+
+// sign in
+document.getElementById("signin").addEventListener("click", noReturn => {
+	// log lout
+});
+
+// forgot password
+document.getElementById("forgot_pass").addEventListener("click", noReturn => {
+
+});
