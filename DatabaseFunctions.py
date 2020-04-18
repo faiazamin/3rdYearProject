9e02694c0d80a7cead36c31e566cb8840fa7014c
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2020-04-18 20:35:50 +0600

    functions for updating tried and solved field added Comments added

--- DatabaseFunctions.py
+++ DatabaseFunctions.py
@@ -1,9 +1,17 @@
+# Code by: Faiaz Amin Khan
+
+
 import sqlite3
 
 conn = sqlite3.connect('test.db')
 
 cursor = conn.cursor()
 
+
+# will check if the email is not already in the database
+# if in the database then returns None
+# if valid email then makes new entry at the database for it having isverified field false
+# returns email if everything is done perfectly
 
 def signup(name, email, institution, password):
     checkmail = (email,)
@@ -18,6 +26,10 @@ def signup(name, email, institution, password):
         return None
 
 
+# checks if email, pass is true for an entry and also it is verified
+# if fine then returns email
+# else returns None
+
 def signin(email, password):
     parameter = (email, password,)
     cursor.execute('SELECT isverified FROM user WHERE email = ? AND password = ?', parameter)
@@ -28,11 +40,16 @@ def signin(email, password):
         return None
 
 
+# makes isVerified field True
+# returns nothing
+
 def verifyUser(email):
     parameter = (email,)
     cursor.execute('UPDATE user SET isverified = 1 WHERE email = ?', parameter)
     conn.commit()
 
+
+# will return the {name} for an email
 
 def getNameAgainstEmail(email):
     parameter = (email,)
@@ -41,11 +58,18 @@ def getNameAgainstEmail(email):
     return rtnMessage[0];
 
 
+# will change the verdict for submissionid
+# returns nothing
+
 def changeVerdict(submissionid, verdict):
     parameter = (verdict, submissionid,)
     cursor.execute('UPDATE submission SET verdict = ? WHERE submissionid = ?', parameter)
     conn.commit()
 
+
+# will create new entry for email and problemid
+# verdict will be "Not Judged Yet"
+# will return the submissionid of the new entry
 
 def newSubmission(email, problemid):
     parameter = ("Not Judged Yet", problemid, email,)
@@ -55,6 +79,9 @@ def newSubmission(email, problemid):
     rtnMessage = cursor.fetchone()
     return rtnMessage[0]
 
+
+# returns a dictionary exactly like down one
+# {"name":"Rahat Hossain", "email" : "rahathossain690@gmail.com", "instritution" : "Univerisity of Dhaka", "solve" : "2", "tried" : "2"}
 
 def userData(email):
     parameter = (email,)
@@ -69,6 +96,10 @@ def userData(email):
     return {"name": rtnMessage1[0], "email": rtnMessage1[1], "institution": rtnMessage1[2], "solved": rtnMessage3[0],
             "tried": rtnMessage2[0]}
 
+
+# if count is False then will return all the submissions
+# if count is True then will return last 30 submissions
+# will return like an array of dictionary of course
 
 def getSubmission(email, count=False):
     parameter = (email,)
@@ -86,6 +117,9 @@ def getSubmission(email, count=False):
         return arrayDictionary
 
 
+# returns all problems exactly like this (array of dictionary)
+# [{"number" : "1", "problemid" : "1000", "name" : "Libir's Wisdom", "tag" : "Beginner", "solved" : }]
+
 def all_problem(email):
     parameter = (email,)
     arrayDictionary = []
@@ -94,7 +128,7 @@ def all_problem(email):
     count = 1
     rtnMessage = cursor.fetchall()
     for row in rtnMessage:
-        if row[3]>0:
+        if row[3] > 0:
             arrayDictionary.append(
                 {"number": count, "problemid": row[0], "name": row[1], "tag": row[2], "solved": "TRUE"})
         else:
@@ -104,14 +138,14 @@ def all_problem(email):
         ++count
     return arrayDictionary
 
-
-def updateTried( problemid ):
+# This function increment the tried field value by 1 for a particular problem
+def updateTried(problemid):
     parameter = (problemid,)
-    cursor.execute('UPDATE problems SET tried = tried + 1 WHERE problemid = ?',parameter)
+    cursor.execute('UPDATE problems SET tried = tried + 1 WHERE problemid = ?', parameter)
     conn.commit()
 
-
-def updateSolved( problemid ):
+# This function increment the solved field value by 1 for a particular problem
+def updateSolved(problemid):
     parameter = (problemid,)
-    cursor.execute('UPDATE problems SET solved = solved + 1 WHERE problemid = ?',parameter)
+    cursor.execute('UPDATE problems SET solved = solved + 1 WHERE problemid = ?', parameter)
     conn.commit()
